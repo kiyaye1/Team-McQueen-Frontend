a29206be5fb7d216416bc3579483ab8de90e29e1
emb9544 <77976029+emb9544@users.noreply.github.com>
2024-02-11 12:21:01 -0500

    I added the image and the first part o f the homepage

--- src/main-screens/Homepage.js
+++ src/main-screens/Homepage.js
@@ -1,10 +1,21 @@
-function Homepage() {
-    return (
-      <><div className="Homepage">
-        <h1> Home</h1>
-      </div></>
-  
-    );
-  }
-  
-  export default Homepage;
+import Button from 'react-bootstrap/Button';
+
+const Homepage = () => {
+  return (
+    <div className="hero bg-cover bg-center h-screen" style={{ backgroundImage: "url(src/img/car.png)" }}>
+      <div className="container flex items-center justify-content-center py-8 px-4 md:px-20">
+        <div className="flex flex-col pr-8 sm:flex-row sm:items-center sm:pr-20 gap-16">
+          <img src="car.png" alt="Image" className="w-64 h-auto ml-auto sm:block" />
+          <div>
+            <h1 className="text-4xl font-bold text-black mb-4">Experience GyroGoGo</h1>
+            <p className="text-xl text-gray-500 leading-loose">Rent an easy-to-drive gyrocar for your local commuting needs.</p>
+            <p className="text-xl text-gray-500 leading-loose mt-2">Become a GyroGoGo member and enjoy the very best way to get around town!</p>
+            <Button href=" " className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">Sign Up</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
