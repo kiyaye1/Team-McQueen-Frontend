154ff36a3bd5f33e4a1c9bcd1b42bdb6a5399d88
emmathomas36 <eet2341@rit.edu>
2024-04-11 11:24:55 -0400

    tweaks to employee page and customer - still need to make edit button function

--- src/main-screens/Profile.js
+++ src/main-screens/Profile.js
@@ -44,62 +44,64 @@ function Profile() {
 
   if (user.role === 0) {
     return (
-      <div className="m-16 grid grid-cols-3 gap-8">
-        {/* Profile Information */}
-        <div class = "col-span-1 space-y-4">
-          <h1 className="text-section-head">Your Account</h1>
-          <div className="text-body-copy">
-            <h5><span class = 'font-bold'>Name:</span> {user.firstName + " " + user.lastName}</h5>
-            <h5><span class = 'font-bold'>Email:</span> {user.emailAddress}</h5>
-            <h5><span class = 'font-bold'>Phone Number:</span> {user.xtra}</h5>
+      <div class = "m-16 space-y-8">
+        <h1 class = "text-section-head">Your Account</h1>
+        <div className="grid grid-cols-3 gap-8">
+          {/* Profile Information */}
+          <div class = "col-span-1 space-y-4">
+            <div className="text-body-copy">
+              <h5><span class = 'font-bold'>Name:</span> {user.firstName + " " + user.lastName}</h5>
+              <h5><span class = 'font-bold'>Email:</span> {user.emailAddress}</h5>
+              <h5><span class = 'font-bold'>Phone Number:</span> {user.xtra}</h5>
+            </div>
+            {/* Button Group - logout and edit */}
+            <div class = "space-x-2">
+              <Button variant="contained" sx={{ backgroundColor: "#000180", foregroundColor: "#FFFFFF", marginLeft: "0em"}} onClick = { () => {logout();}}>Log Out</Button>
+              <Button variant="outlined" sx={{ borderColor: "#000180", color: "#000180", marginLeft: "0em"}} onClick = { () => {editProfile();}}>Edit Profile</Button>
+            </div>
           </div>
-          {/* Button Group - logout and edit */}
-          <div class = "space-x-2">
-            <Button variant="contained" sx={{ backgroundColor: "#000180", foregroundColor: "#FFFFFF", marginLeft: "0em"}} onClick = { () => {logout();}}>Log Out</Button>
-            <Button variant="outlined" sx={{ borderColor: "#000180", color: "#000180", marginLeft: "0em"}} onClick = { () => {editProfile();}}>Edit Profile</Button>
-          </div>
+
+          {/* Reservations */}
+          <div class = "col-span-2 grid grid-cols-1 gap-4">
+            <h3 className="text-card-title">Reservations</h3>
+            {reservations?.filter(data => data.customer.customerID === user.userID).map((data, key) => {
+              dayjs.extend(LocalizedFormat)
+              const startDate = dayjs(data.scheduledStartDatetime).format('LLL')
+              const endDate = dayjs(data.scheduledEndDatetime).format('LLL')
+              
+              return (
+                  <div className="border border-border p-4 rounded-xl" key={key}>
+                    {dayjs().isBefore(startDate) && (
+                      <div class = "pb-2">
+                        <p class = "text-blue-primary font-bold text-sm">Upcoming</p>
+                      </div>
+                    )}
+                    <h4 class = "font-bold">{String(startDate)} - {String(endDate)}</h4>
+                    <p class = "text-body-copy"><span class = "font-bold">Reservation Number:</span> {data.reservationID}</p>
+                    <p class = "text-body-copy"><span class = "font-bold">Start:</span> {data.startStation.name} - {data.startStation.city} {data.startStation.state}</p>
+                    <p class = "text-body-copy"><span class = "font-bold">End:</span> {data.endStation.name} - {data.endStation.city} {data.endStation.state}</p>
+
+                </div>
+
+              );
+            })}   
+
+          </div>     
         </div>
-
-        {/* Reservations */}
-        <div class = "col-span-2 my-4 grid grid-cols-1 gap-4">
-          <h3 className="text-card-title pt-8">Reservations</h3>
-          {reservations?.filter(data => data.customer.customerID === user.userID).map((data, key) => {
-            dayjs.extend(LocalizedFormat)
-            const startDate = dayjs(data.scheduledStartDatetime).format('LLL')
-            const endDate = dayjs(data.scheduledEndDatetime).format('LLL')
-            
-            return (
-                <div className="border border-border p-4 rounded-xl" key={key}>
-                  {dayjs().isBefore(startDate) && (
-                    <div class = "pb-2">
-                      <p class = "text-blue-primary font-bold text-sm">Upcoming</p>
-                    </div>
-                  )}
-                  <h4 class = "font-bold">{String(startDate)} - {String(endDate)}</h4>
-                  <p class = "text-body-copy">Reservation ID: {data.reservationID}</p>
-                  <p class = "text-body-copy">Start: Station {data.startStation.stationID}: {data.startStation.city}, {data.startStation.state}</p>
-                  <p class = "text-body-copy">End: Station {data.endStation.stationID}: {data.endStation.city}, {data.endStation.state}</p>
-
-              </div>
-
-            );
-          })}   
-
-        </div>     
       </div>
     );
   } else if (user.role > 0) {
     return (
       <>
-      <div className="Profile">
+      <div className="Profile m-8">
+        <h2 class = "text-section-head mx-8 mt-8">Your Account</h2>
         <div className="profile-container">
           <div className="profile">
-            <div className="profile-picture"></div>
-            <div className="profile-details">
-              <h1>{`${user.firstName} ${user.lastName}`}</h1>
-              <p>{'Employee #: ' + user.userID}</p>
-              <p>{user.emailAddress}</p>
-              <p>{user.xtra}</p> 
+            <div className="text-body-copy">
+              <p><span class = 'font-bold'>Name:</span> {user.firstName + " " + user.lastName}</p>
+              <p><span class = 'font-bold'>Employee Number:</span> {user.userID}</p>
+              <p><span class = 'font-bold'>Email:</span> {user.emailAddress}</p>
+              <p><span class = 'font-bold'>Role:</span> {user.xtra}</p> 
             </div>
           </div>
         </div>
