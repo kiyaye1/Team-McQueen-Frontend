c98c5a0a4aa7555c9f3ef94090808ebd90847e7e
kiyaye1 <36045045+kiyaye1@users.noreply.github.com>
2024-04-12 02:36:09 -0400

    email verification is implemented.

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -24,7 +24,7 @@ import ReservationConfirmation from '../main-screens/ReservationConfirmation';
 import InactivityDetector from '../hooks/InactivityDetector';
 import FleetManagement from '../employee-pages/FleetManagement';
 import EmployeeManagement from '../employee-pages/EmployeeManagement';
-import MechanicFunctions from '../employee-pages/MechanicFunction';
+import MechanicServiceRequests from '../employee-pages/employee-components/MechanicServiceRequests'
 import MetricsDashboard from '../employee-dashboards/MetricsDashboard';
 import ApplicationMetrics from '../employee-pages/employee-components/ApplicationMetrics';
 import RentalMetrics from '../employee-pages/employee-components/RentalMetrics';
@@ -75,17 +75,16 @@ function Navigation() {
                         <Route path = "reservation-confirmation" element = {employeeRoleNum != 0 ? <Unauthorized /> : <ReservationConfirmation />}/>                        
                     </Route>
                     <Route element={<EmployeeRoute />}>
-                        <Route path = "dash" element = {<FullDashboard employeeRole = {employeeRoleNum}/>}/>
+                    <Route path = "dash" element = {<FullDashboard employeeRole = {employeeRoleNum}/>}/>
                         <Route path = "metrics" element = {<MetricsDashboard />}/>
                         <Route path = "app-metrics" element = {<ApplicationMetrics />}/>
                         <Route path = "rental-metrics" element = {<RentalMetrics />}/>
                         <Route path = "customer-approval/:tab" element = {employeeRoleNum != 3 ? <CustomerServiceFunctions/> : <Unauthorized />}/>
                         <Route path = "customer-details/:id" element = {employeeRoleNum != 3 ? <CustomerDetails/> : <Unauthorized />}/>
                         <Route path = "approval-details/:id" element = {employeeRoleNum != 3 ? <ApprovalDetails/> : <Unauthorized />}/>
-                        <Route path = "fleet-management" element={employeeRoleNum != 3 ? <FleetManagement /> : <Unauthorized /> }/>
+                        <Route path = "fleet-management" element={employeeRoleNum != 2 && employeeRoleNum != 4 ? <FleetManagement /> : <Unauthorized /> }/>
                         <Route path = "employee-management" element = {employeeRoleNum != 2 && employeeRoleNum != 3 ? <EmployeeManagement/> : <Unauthorized />}/>  
-                        <Route path = "mechanic-functions/:tab" element = {employeeRoleNum == 3 ? <MechanicFunctions/> : <Unauthorized/>}/>                   
-                     
+                        <Route path = "service-requests" element = {employeeRoleNum != 2 && employeeRoleNum != 4 ? <MechanicServiceRequests/> : <Unauthorized/>}/>                   
                     </Route>
                     <Route path = "faq" element = {<Faq/>}/>
                     <Route path = "contact" element = {<Contact/>}/>
