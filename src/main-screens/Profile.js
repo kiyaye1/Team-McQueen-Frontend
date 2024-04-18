67e203e12a3a5a57c83639cee38d16e820079d98
emmathomas36 <eet2341@rit.edu>
2024-04-18 14:45:49 -0400

    set up dialog to edit profile info. waiting on an answer from juli for clarification

--- src/main-screens/Profile.js
+++ src/main-screens/Profile.js
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -14,6 +14,9 @@ function Profile() {
   const navigate = useNavigate();
   const [customer, setCustomer] = useState();
   const [reservations, setReservations] = useState([]);
+  const [openEdit, setOpenEdit] = useState(false)
+  const [newEmail, setNewEmail] = useState("")
+  const [newPhone, setNewPhone] = useState("")
 
   useEffect(() => {
     if (user.role === 0) {
@@ -29,9 +32,22 @@ function Profile() {
       .catch(error => console.log(error));
   }
 
-  function editProfile() {
-    // todo
+  const handleClose = () => {
+    setOpenEdit(false)
   }
+
+  const handleEmailChange = (e) => {
+    setNewEmail(e.target.value)
+  }
+
+  const handlePhoneNumberChange = (e) => {
+    setNewPhone(e.target.value)
+  }
+
+  const handleEdit = () => {
+   // handle edit 
+  }
+  
 
   if (user.role === 0) {
     return (
@@ -48,7 +64,7 @@ function Profile() {
          
           <div class = "space-x-2">
             <Button variant="contained" sx={{ backgroundColor: "#000180", foregroundColor: "#FFFFFF", marginLeft: "0em"}} onClick = { () => {logout();}}>Log Out</Button>
-            <Button variant="outlined" sx={{ borderColor: "#000180", color: "#000180", marginLeft: "0em"}} onClick = { () => {editProfile();}}>Edit Profile</Button>
+            <Button variant="outlined" sx={{ borderColor: "#000180", color: "#000180", marginLeft: "0em"}} onClick = { () => {setOpenEdit(true)}}>Edit Profile</Button>
           </div>
 
          </div>
@@ -67,6 +83,16 @@ function Profile() {
                         <p class = "text-blue-primary font-bold text-sm">Upcoming</p>
                       </div>
                     )}
+                    {dayjs().isAfter(startDate) && (
+                      <div class = "pb-2">
+                        <p class = "text-blue-primary font-bold text-sm">Completed</p>
+                      </div>
+                    )}
+                    {dayjs().isAfter(startDate) && dayjs().isBefore(endDate) &&(
+                      <div class = "pb-2">
+                        <p class = "text-blue-primary font-bold text-sm">In Progress</p>
+                      </div>
+                    )}
                     <h4 class = "font-bold">{String(startDate)} - {String(endDate)}</h4>
                     <p class = "text-body-copy"><span class = "font-bold">Reservation Number:</span> {data.reservationID}</p>
                     <p class = "text-body-copy"><span class = "font-bold">Start:</span> {data.startStation.name} - {data.startStation.city} {data.startStation.state}</p>
@@ -78,7 +104,35 @@ function Profile() {
           }
         </div>
         </div>
-        </div>
+
+          <Dialog open = {openEdit} onClose = {handleClose}>
+            <DialogTitle>Edit Profile Details</DialogTitle>
+            <DialogContent>
+                <TextField 
+                autoFocus 
+                margin="dense" 
+                name="emailAddress" 
+                label="Email Address" 
+                fullWidth
+                value={newEmail} 
+                onChange={handleEmailChange} /> 
+                
+                <TextField 
+                autoFocus 
+                margin="dense" 
+                name="name" 
+                label="Station Name" 
+                fullWidth 
+                value={newPhone} 
+                onChange={handlePhoneNumberChange} /> 
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={() => { handleEdit(); handleClose();}} color="primary">Edit Information</Button>
+            </DialogActions>
+          </Dialog>
+
+      </div>
     );
   } else if (user.role > 0) {
     return (
